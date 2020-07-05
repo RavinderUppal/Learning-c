1d6edebc9ded66833ac596738208535991862915
RavinderUppal <67691843+RavinderUppal@users.noreply.github.com>
2020-07-05 14:22:56 +0530

    created product.cpp
    This file give product of two numbers.

---
+++ product.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a,b;
+    cout<<"ENTER TWO NUMBERS"<<endl;
+    cout<<"First: ";
+    cin>>a;
+    cout<<"Second: ";
+    cin>>b;
+    cout<<"Product = "<<a*b<<endl;
+    return 0;
+}
